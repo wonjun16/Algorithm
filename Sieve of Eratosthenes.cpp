638cb0aeaff7a79b9dbde4f55a88a2f5d11aea02
wonjun16 <jun9816@naver.com>
2022-08-21 00:42:34 +0900

    Sieve of Eratosthenes ver_1

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#define SIZE 100000
+int number[SIZE+1];
+
+void sieve() {
+	for (int i = 1; i < SIZE + 1; i++)
+		number[i] = i;
+	for (int i = 2; i * i < SIZE + 1; i++) {
+		if (number[i]) {
+			for (int j = i + 1; j < SIZE + 1; j++) {
+				if (!(number[j] % i)) number[j] = 0;
+			}
+		}
+	}
+	for (int i = 2; i < SIZE + 1; i++) {
+		if (number[i]) std::cout << number[i] << ' ';
+	}
+}
+
+void main() {
+	sieve();
+}
